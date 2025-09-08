0eb37428a307c279d9ffde48c44a7853b9af4fb2
Bellam Rahul <142339913+rahul-bellam@users.noreply.github.com>
2025-09-08 10:55:15 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-<PASTE THE FULL index.js CONTENT FROM ABOVE>
+import React from "react";
+import ReactDOM from "react-dom/client";
+import DailyTaskPlanner from "./components/DailyTaskPlanner";
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <DailyTaskPlanner />
+  </React.StrictMode>
+);
